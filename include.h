995bfd09fcf39f35233c1d9ac8f97b78cc343007
Patricia Liu <patricialiu1168@gmail.com>
2021-05-04 22:45:16 -0400

    initial commit

---
+++ include.h
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
